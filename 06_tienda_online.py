8331bb26f1c3b38cbeff41605cdc7a0d0642131b
gianpc <gpaucarcortez@gmail.com>
2025-03-07 15:09:04 -0500

    feat: add online store system

---
+++ 06_tienda_online.py
@@ -0,0 +1,13 @@
+print("*** Sistema de Tienda Online ***")
+
+# Definimos las variables
+nombre_producto = "Cámra digital"
+precio_producto = 399.99
+cantidad_inventario = 20
+disponible = True
+
+# Mostrar el detalle del producto
+print("Producto:", nombre_producto)
+print("Precio: $", precio_producto)
+print("Cantidad inventario:", cantidad_inventario)
+print("Disponible:", disponible)
