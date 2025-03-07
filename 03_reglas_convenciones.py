b684addc09dc95f35f274eeba961bee1d1a1711d
gianpc <gpaucarcortez@gmail.com>
2025-03-07 14:34:25 -0500

    docs(conventions): add Python naming and formatting rules

---
+++ 03_reglas_convenciones.py
@@ -0,0 +1,21 @@
+# Regla y convenciones en nombre de variables
+
+# Ejemplos de reglas estrictas
+nombre_usuario = 'Juan Perez'
+# 1nombre = 'Juan Perez' # Error de sintaxis
+
+# No podemos usar palabras reservadas
+# class = 'Mi clase' # Error de sintaxis
+
+# Sensibles a mayusculas y minúsculas
+nombre = 'Juan Perez'
+Nombre = 'Laura'
+print(nombre)
+print(Nombre)
+
+# snake_case
+nombre_usuario = 'Juan Perez'
+
+# prefijos y sufijos
+es_casado = True
+nombre_txt = 'archivo.txt'
