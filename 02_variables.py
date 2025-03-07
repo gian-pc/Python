73b71da877d47bf90670363985983f0a78bec6bf
gianpc <gpaucarcortez@gmail.com>
2025-03-07 14:32:09 -0500

    feat(variables): add examples of variable declaration and data types

---
+++ 02_variables.py
@@ -0,0 +1,27 @@
+# Variables en Python
+
+# Declaración e inicialización de variables
+edad = 28
+altura = 1.75
+pais = "Perú"
+
+# Acceder a las variables
+print('Valores iniciales:')
+print('Edad:' , edad)
+print('Altura:', altura)
+print('País:', pais)
+
+# Modificar el valor de una variable
+edad = 30
+altura = 1.80
+
+# Acceder a las variables
+print('---------------------------------')
+print('Valores modificados:')
+print('Edad:' , edad)
+print('Altura:', altura)
+
+# En python el tipo es dinámico
+edad = "Treinta"
+print('---------------------------------')
+print('Edad:', edad)
