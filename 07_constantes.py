dfed8ecef4b16b106235827a3f397970b9bd1eb3
gianpc <gpaucarcortez@gmail.com>
2025-03-07 17:32:17 -0500

    feat: add constants example in Python

---
+++ 07_constantes.py
@@ -0,0 +1,11 @@
+print('*** Constantes en Python ***')
+
+PI = 3.1416
+print('Valor de PI:', PI)
+
+NOMBRE_BASE_DATOS = 'clientes_db'
+print('Nombre de la base de datos:', NOMBRE_BASE_DATOS)
+
+# Aunque python NO lo prohibe, se recomienda no modificar el valor de una constante
+NOMBRE_BASE_DATOS = 'productos_db'
+print('Nombre de la base de datos:', NOMBRE_BASE_DATOS)
