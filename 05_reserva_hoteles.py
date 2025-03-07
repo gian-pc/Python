be70b39de6c48533a9f0bbcf46e59518e2643b01
gianpc <gpaucarcortez@gmail.com>
2025-03-07 15:00:03 -0500

    feat: add hotel reservation details

---
+++ 05_reserva_hoteles.py
@@ -0,0 +1,14 @@
+print("*** Sistema de Reserva de Hoteles ***")
+# Definimos las variables
+cliente = 'Laura Martínez'
+dias_estancia = 5
+tarifa_diaria = 1200.0
+vista_al_mar = True
+
+# Mostrar el detalle de la reserva
+print("Cliente:", cliente)
+print("Días de estancia:", dias_estancia)
+print("Tarifa diaria:", tarifa_diaria)
+print("Habitación con vista al mar?:", vista_al_mar)
+
+#
