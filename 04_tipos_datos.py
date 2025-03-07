93ce22f612a13ce37390f4e7a819b2a1bd0d80e6
gianpc <gpaucarcortez@gmail.com>
2025-03-07 14:46:45 -0500

    feat: add data types examples

---
+++ 04_tipos_datos.py
@@ -0,0 +1,21 @@
+# Ejemplo de tipos de datos en Python
+
+# Entero
+edad = 30
+print(edad)
+
+# Numero con puntos flotante
+altura = 1.75
+print('Altura:', altura)
+
+# Cadena de texto
+nombre = 'Ana'
+print('Nombre:', nombre)
+
+# Tipo Booleano
+es_estudiante = True
+print('Es estudiante:', es_estudiante)
+
+# Tipo None, ausencia de valor
+direccion = None
+print('Dirección:', direccion)
