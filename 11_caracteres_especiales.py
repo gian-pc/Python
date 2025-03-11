31cb6e73639e5010885ed294c68a818f1f507fe9
gianpc <gpaucarcortez@gmail.com>
2025-03-11 11:40:19 -0500

    feat: agregar ejemplos de caracteres especiales en Python

---
+++ 11_caracteres_especiales.py
@@ -0,0 +1,6 @@
+# Caracteres Especiales
+print('Hola \nMundo') # Salto de línea
+print('\t\tHola \tMundo') # Tabulación
+print('Hola \bMundo') # Retroceso
+print('Juan \' "Pérez') # Comillas simples y dobles
+print('Caracter \\ diagonal invertida') # Diagonal invertida
