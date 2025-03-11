629ee9ff2f8fcb4337aca892c865317a449431ed
gianpc <gpaucarcortez@gmail.com>
2025-03-11 15:33:28 -0500

    feat: agregar ejemplos de reemplazo de subcadenas

---
+++ 18_reemplazar_subcadenas.py
@@ -0,0 +1,10 @@
+# Reemplazar subcadenas
+
+cadena = 'Hola, mundo!'
+print(f'Cadena original: {cadena}')  # Hola, mundo!
+nueva_cadena =  cadena.replace('mundo', 'a todos')
+print(f'Nueva cadena: {nueva_cadena}') # Hola, a todos!
+
+# Sustituir hola por adios
+nueva_cadena = cadena.replace('Hola', 'Adios')
+print(f'Nueva cadena: {nueva_cadena}')  # Adios, mundo!
