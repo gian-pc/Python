5d53051e5744f77278f7a3907eee67bd9c4084bd
gianpc <gpaucarcortez@gmail.com>
2025-03-11 14:18:59 -0500

    feat: agregar ejemplos de manejo de subcadenas

---
+++ 16_subcadenas.py
@@ -0,0 +1,9 @@
+# Manejo de subcadenas
+cadena = 'Hola, mundo!'
+
+# Obtenemos la subcadena de hola [inicio:fin (sin incluir)]
+subcadena = cadena[0:4]
+print(f'Subcadena de hola: {subcadena}')
+
+subcadena_mundo = cadena[6:11]
+print(f'Subcadena de mundo: {subcadena_mundo}')
