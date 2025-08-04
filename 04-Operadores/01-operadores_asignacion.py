aca9c60fefd3c0f612396acc4aa55855ac23cd06
gianpc <gpaucarcortez@gmail.com>
2025-08-04 14:34:07 -0500

    feat: ejemplo de asignación multiple

--- 04-Operadores/01-operadores_asignacion.py
+++ 04-Operadores/01-operadores_asignacion.py
@@ -11,3 +11,15 @@ print(f'Asignación multiple: a={a}, b={b}, c={c}')
 # Asignación encadenada
 contador_1 = contador_2 = 0
 print(f'Asignación encadenada: contador_1={contador_1}, contador_2={contador_2}')
+
+# Intercambio de valores de una variable, sin utilizar variables temporales
+x, y = 5, 10
+print(f'Valores iniciales x={x}, y={y}')
+
+# Aplicando el concepto de asignación multiple, intercambiamos valores
+x, y = y, x
+print(f'Valores iniciales x={x}, y={y}')
+
+# Recibir multiples valores de la entrada del usuario
+nombre, apellido = input('Ingresa tu nombre y apellido separador por coma: ').split(',')
+print(f'Nombre: {nombre.strip()}, Apellido: {apellido.strip()}')
