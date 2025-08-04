5192e393e3bb23e6e897fb31965d3550ee7b43b2
gianpc <gpaucarcortez@gmail.com>
2025-08-04 13:03:22 -0500

    feat: operadores aritméticos

---
+++ 04-Operadores/00-operadores_aritmeticos.py
@@ -0,0 +1,32 @@
+#  Operadores Aritméticos
+
+a = 10
+b = 3
+
+# Suma
+suma = a + b
+print(f'Suma de {a} + {b}: {suma}')
+
+# Resta
+resta = a - b
+print(f'Resta de {a} - {b}: {resta}')
+
+# Multiplicación
+multiplicacion = a * b
+print(f'Multiplicacion de {a} * {b}: {multiplicacion}')
+
+# División (retorna un tipo float)
+division = a / b
+print(f'Division de {a} / {b}: {division:.2f}')
+
+# División Entera
+division_entera = a // b
+print(f'Division entera {a} // {b}: {division_entera}')
+
+# Modulo (residuo de la división)
+modulo = a % b
+print(f'Modulo de {a} % {b}: {modulo}')
+
+# Exponente
+exponente = a ** b
+print(f'Exponente de {a} ** {b}: {exponente}')
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 1. Variables
 2. Manejo de Cadenas en Python
 3. Entrada de Datos en Python
+4. Operadores en Python
