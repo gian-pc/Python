735ef2e02a43c4ff4ab3e5d19c10e4bb21b8d290
gianpc <gpaucarcortez@gmail.com>
2025-08-04 14:49:04 -0500

    feat: operadores de asignación compuesta

---
+++ 04-Operadores/02-operadores_compuestos.py
@@ -0,0 +1,33 @@
+# Operadores de asignación compuesta
+
+# Operador compuesto de suma
+a = 4
+b = 2
+
+a += 1
+b += 2
+print(f'a= {a}, b= {b}')
+
+# Operador compuesto de resta
+c = 3
+d = 1
+
+c -= 1
+d -= 2
+print(f'a= {c}, b= {d}')
+
+# Operador compuesto de multiplicación
+e = 3
+f = 2
+
+e *= 1
+f *= 2
+print(f'a= {e}, b= {f}')
+
+# Operadores compuesto de división
+g = 5
+h = 2
+
+g /= 5
+h /= 2
+print(f'a= {g}, b= {h}')
