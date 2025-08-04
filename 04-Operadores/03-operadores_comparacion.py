bcf21f4c8ff4b3aa2ab0f50d1f323e0d4cdf7d52
gianpc <gpaucarcortez@gmail.com>
2025-08-04 15:17:40 -0500

    feat: operadores de comparación

---
+++ 04-Operadores/03-operadores_comparacion.py
@@ -0,0 +1,26 @@
+print('*** Operadores Comparación (Relacionales) ***')
+a, b = 3, 5
+
+# Operador igualdad ==
+resultado = a == b
+print(f'Resulatdo a == b es: {resultado}')
+
+# Operador diferente !=
+resultado = a != b
+print(f'Resultado a != b es: {resultado}')
+
+# Operador mayor que >
+resultado = a > b
+print(f'Resultado a > b es: {resultado}')
+
+# Operador mayor o igual que >=
+resultado = a >= b
+print(f'Resultado a >= b es: {resultado}')
+
+# Operador menor que <
+resultado = a <= b
+print(f'Resultado a <= b es: {resultado}')
+
+# Operador menor o igual que <=
+resultado = a <= b
+print(f'Resultado a <= b es: {resultado}')
