d2741589d802fca5b2ab7f11a8909ff931dd176b
gianpc <gpaucarcortez@gmail.com>
2025-08-04 10:53:00 -0500

    feat: recetas de cocina

---
+++ 03- Entrada-de-datos/05-recetas_cocina.py
@@ -0,0 +1,12 @@
+print('*** Receta de  Cocina ***')
+
+plato = input('Ingresa el nombre: ')
+ingredientes = input('Ingresa los ingredientes: ')
+tiempo = int(input('Ingresa el tiempo de preparación (min): '))
+dificultad = input('Ingresa la dificultad: ')
+
+print('-------------------')
+print(f'Nombre receta: {plato}')
+print(f'Ingredientes: {ingredientes}')
+print(f'Tiempo de preparación: {tiempo}')
+print(f'Dificultad: {dificultad}')
