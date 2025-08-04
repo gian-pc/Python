dee774fce7183ba06999a13fca420d60e99391f2
gianpc <gpaucarcortez@gmail.com>
2025-08-04 15:46:28 -0500

    feat: operador or

---
+++ 04-Operadores/06-operador_or.py
@@ -0,0 +1,9 @@
+from idlelib.debugger_r import restart_subprocess_debugger
+
+print('*** Operador lógico or ***')
+condicion_1 = False
+condicion_2 = True
+
+# El operador OR regresa True --> Si cualquiera de los operandos es True
+resultado = condicion_1 or condicion_2
+print(f'Resultado {condicion_1} or {condicion_2} es: {resultado}')
