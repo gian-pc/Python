8a23a6cb8e5ebd2231c1a334a76fa55315e8ee91
gianpc <gpaucarcortez@gmail.com>
2025-08-04 15:29:08 -0500

    feat: operador and

---
+++ 04-Operadores/04-operador_and.py
@@ -0,0 +1,7 @@
+print('*** Operador AND ***')
+
+# T and T = True
+condicion_1 = True
+condicion_2 = True
+resultado = condicion_1 and condicion_2
+print(f'Resulatdo {condicion_1} and {condicion_2}: {resultado}')
