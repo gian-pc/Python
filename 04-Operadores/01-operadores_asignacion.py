21b773ef46a28e451dd13403d701b0c20f245c00
gianpc <gpaucarcortez@gmail.com>
2025-08-04 14:20:45 -0500

    feat: operadores de asignación

---
+++ 04-Operadores/01-operadores_asignacion.py
@@ -0,0 +1,13 @@
+# Operadores de Asignacion
+
+# Asignación simple
+numero = 5
+print(f'Asignación simple: {numero}')
+
+# Asignación multiple
+a, b, c = 'Hola', 2, 9.3
+print(f'Asignación multiple: a={a}, b={b}, c={c}')
+
+# Asignación encadenada
+contador_1 = contador_2 = 0
+print(f'Asignación encadenada: contador_1={contador_1}, contador_2={contador_2}')
