94266d61d0816ed78a2242b89ab68485a397aa31
gianpc <gpaucarcortez@gmail.com>
2025-08-04 00:25:15 -0500

    feat: concatenación y suma de valores

---
+++ 03- Entrada-de-datos/01-ejemplo_tipos_datos.py
@@ -0,0 +1,16 @@
+# Ejemplo tipos datos
+
+# Ejemplo de concatenación o suma de valores
+numero1_cadena = '10'
+numero2_cadena = '20'
+print(f'Número en cadena: {numero1_cadena}')
+print(f'Número en cadena: {numero2_cadena}')
+
+suma = numero1_cadena + numero2_cadena
+print(f'Valor suma de numeros: {suma}')
+
+# Convertimos a tipos enteros
+numero1_entero = int(numero1_cadena)
+numero2_entero = int(numero2_cadena)
+suma = numero1_entero + numero2_entero
+print(f'Valor suma de numeros: {suma}')
