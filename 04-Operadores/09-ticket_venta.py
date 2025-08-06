6b1500d5032ac1a750b913dc2dc70c877565b7b1
gianpc <gpaucarcortez@gmail.com>
2025-08-05 23:55:13 -0500

    feat: ticket de venta con descuento

--- 04-Operadores/09-ticket_venta.py
+++ 04-Operadores/09-ticket_venta.py
@@ -4,11 +4,25 @@ precio_leche = float(input('Precio leche: '))
 precio_pan = float(input('Precio pan: '))
 precio_lechuga = float(input('Precio lechuga: '))
 precio_platano = float(input('Precio platano: '))
+descuento_porcentaje = int(input('Aplicar algún descuento (%)?: '))
 
+# Calculo del subtotal (sin impuestos)
 subtotal = precio_leche + precio_pan + precio_lechuga  + precio_platano
-igv = subtotal * 0.16
-total = subtotal + igv
+
+# Aplicar el descuento
+descuento = subtotal * descuento_porcentaje/100
+
+# Subtotal con descuento
+subtotal_con_descuento = subtotal - descuento
+
+# Calculo con impuestos (16%)
+igv = subtotal_con_descuento * 0.16
+
+# Calculo total de la compra (con impuestos)
+total = subtotal_con_descuento + igv
 
 print(f'''\nSubtotal: ${subtotal:.2f}
+Descuento: ${descuento:.2f} ({descuento_porcentaje}%)
+Subtotal con descuento: ${subtotal_con_descuento:.2f}
 Impuesto (16%): ${igv:.2f}
 Costo Total de la compra: ${total:.2f}''')
