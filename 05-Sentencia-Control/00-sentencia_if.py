ce25d12fedb90a370bddf801dc714232068c8d59
gianpc <gpaucarcortez@gmail.com>
2025-08-06 12:02:52 -0500

    feat: sentencia elif

--- 05-Sentencia-Control/00-sentencia_if.py
+++ 05-Sentencia-Control/00-sentencia_if.py
@@ -1,8 +1,10 @@
 print('*** Sentencia IF ***')
 
-edad = 8
+edad = 16
 
 if edad >= 18:
     print(f'Eres mayor de edad. Tienes {edad} años')
+elif 13 <= edad < 18:
+    print(f'Eres un adolescente. Tienes {edad} años')
 else:
     print(f'Eres menor de edad. Tienes {edad} años')
